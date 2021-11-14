fbb67a8cf2c290a8c5e7c43812fcbf22b4cda983
gaogh2 <gaogh2@lenovo.com>
2021-11-14 20:44:19 +0800

    remove forward code

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  var ip = req.headers["true-client-ip"] || req.headers['x-forwarded-for'] || req.socket.remoteAddress
+  var ip = req.headers["true-client-ip"] || req.socket.remoteAddress
   res.render('index', { title: 'Express', ipAddress: ip });
 });
 
